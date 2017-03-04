c6216610bd46e67400ec00b313a8671f73197504
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2017-03-04 13:02:33 +0100

    Complete implementation of conversion for ellipses and polygons.

--- src/js/conversion/generate-shapescript.js
+++ src/js/conversion/generate-shapescript.js
@@ -6,6 +6,39 @@ const parseSvg = function (source) {
     return new DOMParser().parseFromString(source, 'application/xml');
 };
 
+const popToken = function (context) {
+    if (context.tokens.length > 0) {
+        let token = context.tokens[0];
+        context.tokens = context.tokens.splice(1);
+        return token;
+    } else {
+        return undefined;
+    }
+}
+
+const parsePath = function (context) {
+    while (token = popToken(context)) {
+        if (token == 'Z') {
+            // Nothing to do here
+        } else if (token == 'M') {
+            let x = Number(popToken(context));
+            let y = Number(popToken(context));
+            context.instructions.push({fn: 'MoveTo', args: [x, y]});
+            context.scaler.register(x, y);
+        } else if (token == 'L') {
+            let x = Number(popToken(context));
+            let y = Number(popToken(context));
+            context.instructions.push({fn: 'LineTo', args: [x, y]});
+            context.scaler.register(x, y);
+        } else {
+            let x = Number(token);
+            let y = Number(popToken(context));
+            context.instructions.push({fn: 'LineTo', args: [x, y]});
+            context.scaler.register(x, y);
+        }
+    }
+}
+
 const generateShapeScript = function (svgDocument) {
     let defsRemovalInProgress = true;
 
@@ -19,9 +52,10 @@ const generateShapeScript = function (svgDocument) {
         }
     }
 
-    let rects = svgDocument.getElementsByTagName('rect');
     let instructions = [];
     let scaler = new scaling.Scaler();
+
+    let rects = svgDocument.getElementsByTagName('rect');
 
     for (let rectIndex = 0; rectIndex < rects.length; rectIndex++) {
         let rect = rects[rectIndex];
@@ -34,11 +68,42 @@ const generateShapeScript = function (svgDocument) {
         scaler.register(x + w, y + h);
     }
 
+    let ellipses = svgDocument.getElementsByTagName('ellipse');
+
+    for (let ellipseIndex = 0; ellipseIndex < ellipses.length; ellipseIndex++) {
+        let ellipse = ellipses[ellipseIndex];
+        let cx = Number(ellipse.getAttribute('cx'));
+        let cy = Number(ellipse.getAttribute('cy'));
+        let rx = Number(ellipse.getAttribute('rx'));
+        let ry = Number(ellipse.getAttribute('ry'));
+        let x = cx - rx;
+        let y = cy - ry;
+        let w = rx * 2;
+        let h = ry * 2;
+        instructions.push({fn: 'Ellipse', args: [x, y, w, h]});
+        scaler.register(x, y);
+        scaler.register(x + w, y + h);
+    }
+
+    let paths = svgDocument.getElementsByTagName('path');
+
+    for (let pathIndex = 0; pathIndex < paths.length; pathIndex++) {
+        let path = paths[pathIndex];
+        let pathDescription = path.getAttribute('d');
+        let tokens = pathDescription.match(/[\w\.]+/g);
+        parsePath({tokens, instructions, scaler});
+    }
+
     instructions.forEach(function (inst) {
-        inst.args[0] = scaler.scale_x(inst.args[0]);
-        inst.args[1] = scaler.scale_y(inst.args[1]);
-        inst.args[2] = scaler.scale_w(inst.args[2]);
-        inst.args[3] = scaler.scale_h(inst.args[3]);
+        if (inst.fn == 'Rectangle') {
+            inst.args[0] = scaler.scale_x(inst.args[0]);
+            inst.args[1] = scaler.scale_y(inst.args[1]);
+            inst.args[2] = scaler.scale_w(inst.args[2]);
+            inst.args[3] = scaler.scale_h(inst.args[3]);
+        } else {
+            inst.args[0] = scaler.scale_x(inst.args[0]);
+            inst.args[1] = scaler.scale_y(inst.args[1]);
+        }
     });
 
     let text = 'shape main {\n';
--- test/test-shapescript-polygon.js
+++ test/test-shapescript-polygon.js
@@ -3,14 +3,15 @@ let assert = require('assert');
 let fs = require('fs');
 let xmldom = require('xmldom');
 
-describe('Shapescript ellipse parser', function () {
-    it('should generate a simple ellipse', function () {
+describe('Shapescript polygon parser', function () {
+    it('should generate a simple polygon path', function () {
         let parser = new xmldom.DOMParser();
-        let svgText = fs.readFileSync(__dirname + '/test-shapescript-ellipse-h.svg', {encoding: 'utf-8'});
+        let svgText = fs.readFileSync(__dirname + '/test-shapescript-polygon.svg', {encoding: 'utf-8'});
         let svgDocument = parser.parseFromString(svgText);
         let ss = svg2ss.generateShapeScript(svgDocument);
         // console.log(ss);
-        assert.equal(ss.match(/Ellipse\(/g).length, 1);
+        assert.equal(ss.match(/MoveTo\(/g).length, 2);
+        assert.equal(ss.match(/LineTo\(/g).length, 10);
     });
 });
 
